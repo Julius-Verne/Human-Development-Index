22fff8f23005206cde22f01df16f41be7cbe3727
Julius-Verne <juliusvernedesign@gmail.com>
2018-09-24 22:41:46 -0500

    <HDI Datasets>

---
+++ run-all.R
@@ -0,0 +1,6 @@
+library(readxl)
+library(tidyverse)
+library(ggplot2)
+library(here)
+
+source(here("input-data","importing"))
---
+++ scripts/importing.R
@@ -0,0 +1,9 @@
+
+
+HDI_2017 <- read_excel(here("input-data","HDI_1.xlsx"), sheet = "HDI")
+MEAS_2017 <- read_excel(here("input-data","HDI_1.xlsx"), sheet = "Measurements")
+REG_2017 <- read_excel(here("input-data","HDI_1.xlsx"), sheet = "Regions")
+
+HDI_TIME <- read_excel(here("input-data","HDI_2.xlsx"), sheet = "HDI_Time")
+MEAS_TIME <- read_excel(here("input-data","HDI_2.xlsx"), sheet = "Measurements")
+REG_TIME <- read_excel(here("input-data","HDI_2.xlsx"), sheet = "Regions")
